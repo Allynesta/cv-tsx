e91bd66cecc3d7742a4cb47a7631a9bdd7156058
Allynesta <ally_nesta1107@outlook.com>
2024-06-22 14:07:04 +0400

    update redirect of link in nav bar

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -10,10 +10,10 @@ const Navbar = () => {
 				<a className="active" href="#home">
 					Home
 				</a>
-				<a href="skill-section">Skills</a>
-				<a href="experience-section">Experience</a>
-				<a href="about-section">About Me</a>
-				<a href="contact-section">About Me</a>
+				<a href="#skill-section">Skills</a>
+				<a href="#experience-section">Experience</a>
+				<a href="#about-section">About Me</a>
+				<a href="#contact-section">About Me</a>
 			</div>
 		</>
 	);
