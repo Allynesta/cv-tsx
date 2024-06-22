a61afb1de7fb4da7e9ee21f1526c8ea065b6eb03
Allynesta <ally_nesta1107@outlook.com>
2024-06-22 14:28:23 +0400

    matching of color on website

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -3,10 +3,11 @@ const Footer = () => {
 		<footer>
 			<p>Find me on social media.</p>
 			<ul className="no-bullets">
+				<li>Facebook</li>
 				<li>Instagram</li>
 				<li>LinkedIn</li>
 			</ul>
-			<i className="fa fa-copyright">Ally Nesta - Private Property</i>
+			<i className="fa fa-copyright">Ally Nesta - Website Underconstruction</i>
 		</footer>
 	);
 };
