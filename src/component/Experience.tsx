f8207162f81a9cc9cf10c7cdb42a60733269d4c5
Allynesta <ally_nesta1107@outlook.com>
2024-06-22 16:33:04 +0400

    add dynamic to experience section

--- src/component/Experience.tsx
+++ src/component/Experience.tsx
@@ -1,22 +1,44 @@
 import "../styles/experience.css";
+import { useState } from "react";
 
 const Experience = () => {
+	const [expandedPanel, setExpandedPanel] = useState<number | null>(null);
+
+	// Function to toggle the panel visibility
+	const togglePanel = (index: number) => {
+		if (expandedPanel === index) {
+			// If the clicked panel is already expanded, collapse it
+			setExpandedPanel(null);
+		} else {
+			// Expand the clicked panel
+			setExpandedPanel(index);
+		}
+	};
+
 	return (
 		<div className="experience-section">
 			<h4>
 				<b>Working Experience</b>
 			</h4>
-			<button className="accordion">
+			<button
+				className="accordion"
+				onClick={() => togglePanel(0)}
+				aria-expanded={expandedPanel === 0}
+			>
 				Lead Technical Scriptor @ Bilendi Services
 			</button>
-			<div className="panel">
+			<div className={`panel ${expandedPanel === 0 ? "show" : "hide"}`}>
 				<p>Implement new survey project</p>
 				<p>Participate in project launching meeting</p>
 			</div>
-			<button className="accordion">
+			<button
+				className="accordion"
+				onClick={() => togglePanel(1)}
+				aria-expanded={expandedPanel === 1}
+			>
 				Software QA Tester Level 1 @ SGS & CO
 			</button>
-			<div className="panel">
+			<div className={`panel ${expandedPanel === 1 ? "show" : "hide"}`}>
 				<p>
 					Ensuring quality of evolutionary and corrective versions of
 					application
@@ -24,14 +46,24 @@ const Experience = () => {
 				<p>Create user manuals for new features</p>
 				<p>Create and execute test suites/cases</p>
 			</div>
-			<button className="accordion">
+			<button
+				className="accordion"
+				onClick={() => togglePanel(2)}
+				aria-expanded={expandedPanel === 2}
+			>
 				Web developer @ Enabling Environment
 			</button>
-			<div className="panel">
+			<div className={`panel ${expandedPanel === 2 ? "show" : "hide"}`}>
 				<p>Maintenance of company website via WordPress</p>
 			</div>
-			<button className="accordion">Trainee @ ABC Motor LTD</button>
-			<div className="panel">
+			<button
+				className="accordion"
+				onClick={() => togglePanel(3)}
+				aria-expanded={expandedPanel === 3}
+			>
+				Trainee @ ABC Motor LTD
+			</button>
+			<div className={`panel ${expandedPanel === 3 ? "show" : "hide"}`}>
 				<p>Helpdesk support</p>
 				<p>Infrastructure support</p>
 				<p>Networking</p>
