b7f381b06c5942e74871b104f00e5b0725d90c46
Allynesta <ally_nesta1107@outlook.com>
2024-06-22 14:30:41 +0400

    responsive nav bar

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,9 +1,19 @@
+import React, { useState } from "react";
 import "../styles/navbar.css";
 
 const Navbar = () => {
+	const [responsive, setResponsive] = useState(false);
+
+	const toggleResponsive = () => {
+		setResponsive(!responsive);
+	};
+
 	return (
 		<>
-			<div className="topnav-section" id="topnav-section">
+			<div
+				className={`topnav-section ${responsive ? "responsive" : ""}`}
+				id="topnav-section"
+			>
 				<a className="disabledlogo" href="#default">
 					Ally Nesta
 				</a>
@@ -14,6 +24,11 @@ const Navbar = () => {
 				<a href="#skill-section">Skills</a>
 				<a href="#experience-section">Experience</a>
 				<a href="#contact-section">Contact</a>
+
+				{/* Responsive icon */}
+				<a className="icon" onClick={toggleResponsive}>
+					&#9776;
+				</a>
 			</div>
 		</>
 	);
