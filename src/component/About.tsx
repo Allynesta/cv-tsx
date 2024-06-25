812ff0d54239ec60fd10c52ef4c961b0a02e04ce
Allynesta <ally_nesta1107@outlook.com>
2024-06-25 22:55:49 +0400

    update about

--- src/component/About.tsx
+++ src/component/About.tsx
@@ -5,12 +5,11 @@ const About = () => {
 		<>
 			<div className="about-section" id="about-section">
 				<h1>Welcome!</h1>
-				<p>Work Smart</p>
 				<p>
 					I'm Ally Nesta,
 					<br /> and I specialize in Web development with a passion for both
 					frontend and backend,
-					<br /> I'm got 1 motto work smart.
+					<br /> I have only one motto - work smart.
 					<br /> Explore my path and let's connect to.
 				</p>
 			</div>
